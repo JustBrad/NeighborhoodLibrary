3b33f85f0b39b14bfe5562fc8a408902b413dcf5
Brad <bradegbert26@gmail.com>
2023-04-21 12:34:39 -0500

    Initial commit

---
+++ src/main/java/org/yearup/Book.java
@@ -0,0 +1,6 @@
+package org.yearup;
+
+public class Book
+{
+
+}
---
+++ src/main/java/org/yearup/NeighborhoodLibrary.java
@@ -0,0 +1,10 @@
+package org.yearup;
+
+public class NeighborhoodLibrary
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+}
